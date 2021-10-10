63ea439c92f3ee711b93890df28ff74ace25733f
MaxwellJFleming <maxwell.j.fleming@gmail.com>
2021-10-10 16:13:34 -0500

    added Week2Movement

---
+++ Week2Movement/Character.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Week2Movement
+{
+    public class Character
+    {
+        public Texture2D Texture;
+        public Keys JumpButton = Keys.Up;
+        public int JumpPower = 20;
+        public float Gravity = 1.4f;
+        public float Speed = 0f;
+        public Vector2 Pos = new Vector2(0, 0);
+
+        public Character() { }
+
+        public Character(Keys k, int jp, float g, Vector2 v)
+        {
+            JumpButton = k;
+            JumpPower = jp;
+            Gravity = g;
+            Pos = v;
+        }
+
+        public Character(Texture2D t, Keys k, int jp, float g, Vector2 v)
+        {
+            Texture = t;
+            JumpButton = k;
+            JumpPower = jp;
+            Gravity = g;
+            Pos = v;
+        }
+
+        public void UpdateTexture(Texture2D t)
+        {
+            Texture = t;
+        }
+        public void Jump()
+        {
+            Speed = JumpPower;
+        }
+
+        public void UpdatePos(float time)
+        {
+            if (Speed != 0 && Pos.Y != 0)
+            {
+                Speed -= Gravity * time;
+                Vector2 NewPos = new Vector2(0, Pos.Y + Speed);
+                if (NewPos.Y <= 0)
+                    NewPos = new Vector2(0, 0);
+            }
+        }
+    }
+}
---
+++ Week2Movement/Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Week2Movement
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private float screenHeight;
+        private float screenWidth;
+        private SpriteFont Arial;
+        private Texture2D Character;
+
+        private Character player1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            screenHeight = graphics.GraphicsDevice.Viewport.Height;
+            screenWidth = graphics.GraphicsDevice.Viewport.Width;
+            
+            player1 = new Character(Keys.Up, 20, 1.4f, new Vector2(screenWidth/2, screenHeight));
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Arial = Content.Load<SpriteFont>("Arial");
+            Character = Content.Load<Texture2D>("Player");
+            
+            //player1.Texture = Character;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            float time = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(player1.JumpButton) && player1.Pos.Y == 0)
+                player1.Jump();
+            
+            player1.UpdatePos(time);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Character, player1.Pos, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
